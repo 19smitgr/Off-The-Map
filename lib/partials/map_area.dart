0fd5d83c1fbf21880a9f2068fdc66573811f0187
Garrett Smith <garrett.smit7@gmail.com>
2019-06-16 19:35:17 -0400

    Begin work on infowindow

--- lib/partials/map_area.dart
+++ lib/partials/map_area.dart
@@ -15,6 +15,7 @@ class MapArea extends StatefulWidget {
 
 class _MapAreaState extends State<MapArea> {
   bool showInfoWindow = false;
+  InfoWindow infoWindow = InfoWindow(parentLatLng: LatLng(51.5, -0.09));
 
   @override
   Widget build(BuildContext context) {
@@ -27,7 +28,7 @@ class _MapAreaState extends State<MapArea> {
         layers: [
           TileLayerOptions(
             urlTemplate:
-                'https://a.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}',
+                'https://api.mapbox.com/v4/{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}',
             additionalOptions: {
               'accessToken':
                   'pk.eyJ1IjoiMTlzbWl0Z3IiLCJhIjoiY2p3anAxYnBhMHVhcTQ5bXhqOXczMTF4NSJ9.BpUH44ClryfRjH7ivHuL5Q',
@@ -37,28 +38,59 @@ class _MapAreaState extends State<MapArea> {
           MarkerLayerOptions(
             markers: [
               Marker(
-                width: 80.0,
-                height: 80.0,
-                point: LatLng(51.5, -0.09),
-                builder: (ctx) => Container(
-                      child: Icon(Icons.location_on,
-                          size: 50.0, color: kOrangeMarkerColor),
-                    ),
+                point: infoWindow.getOffsetLatLng(),
+                builder: (context) {
+                  return InfoWindow(parentLatLng: LatLng(51.5, -0.09));
+                },
+                width: 200,
+                height: 150
               ),
               if (!showInfoWindow)
                 Marker(
-                  width: 80.0,
-                  height: 80.0,
                   point: LatLng(51.5, -0.09),
                   builder: (ctx) => Container(
-                        child: Icon(Icons.location_on,
-                            size: 60.0, color: Colors.black),
+                        child: Icon(
+                          Icons.location_on,
+                          size: 50.0,
+                          color: kOrangeMarkerColor,
+                        ),
                       ),
                 ),
             ],
           ),
         ],
       ),
+    );
+  }
+}
+
+class InfoWindow extends StatefulWidget {
+  // trying to center infowindow
+  // TODO: find a better way to do this exactly and make sure it works on different screen sizes
+  static const offsetFromMarker = Offset(0.01, 0.002);
+
+  final LatLng parentLatLng;
+
+  InfoWindow({this.parentLatLng});
+
+  LatLng getOffsetLatLng() {
+    LatLng latLng = LatLng(0, 0);
+
+    latLng.latitude = parentLatLng.latitude + InfoWindow.offsetFromMarker.dx;
+    latLng.longitude = parentLatLng.longitude + InfoWindow.offsetFromMarker.dy;
+
+    return latLng;
+  }
+
+  @override
+  _InfoWindowState createState() => _InfoWindowState();
+}
+
+class _InfoWindowState extends State<InfoWindow> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: kOrangeMarkerColor,
     );
   }
 }
